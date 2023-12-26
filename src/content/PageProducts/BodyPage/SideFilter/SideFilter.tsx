1dcace493d65fc1a57f76215edc8af3fb53604f4
hatem782 <hatembenechikh100@gmail.com>
2023-12-26 18:27:32 +0100

    stock filter

--- src/content/PageProducts/BodyPage/SideFilter/SideFilter.tsx
+++ src/content/PageProducts/BodyPage/SideFilter/SideFilter.tsx
@@ -3,11 +3,13 @@ import Paper from "@/components/Paper/Paper";
 import styles from "./SideFilter.module.scss";
 
 import CategsGroup from "./CategsGroup/CategsGroup";
+import StockGroup from "./StockGroup/StockGroup";
 
 function SideFilter() {
   return (
     <Paper className={styles.main}>
       <CategsGroup />
+      <StockGroup />
     </Paper>
   );
 }
